556a1306c270d81d7225679060a4844931331f66
mvpdz1 <30734057+mvpdz1@users.noreply.github.com>
2025-08-20 20:51:31 +0800

    Update AndroidExtentions.kt

--- fetch2core/src/main/java/com/tonyodev/fetch2core/AndroidExtentions.kt
+++ fetch2core/src/main/java/com/tonyodev/fetch2core/AndroidExtentions.kt
@@ -31,7 +31,6 @@ fun Context.isNetworkAvailable(): Boolean {
         val network = connectivityManager.activeNetwork ?: return false
         val networkCapabilities = connectivityManager.getNetworkCapabilities(network) ?: return false
         return networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
-                && networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED)
     } else {
         val activeNetworkInfo = connectivityManager.activeNetworkInfo
         return activeNetworkInfo != null && activeNetworkInfo.isConnected
